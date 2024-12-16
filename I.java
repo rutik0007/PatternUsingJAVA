92e228f241fc955f1ab265e583400c9b7512f97b
rutik0007 <rutikbadhe77@gmail.com>
2024-12-16 13:47:29 +0530

    Update I.java

--- I.java
+++ I.java
@@ -1,8 +1,8 @@
 
 public class I {
 	public static void main(String[] args) {
-		for (int i = 0; i < 7; i++) { // Outer loop for rows
-			for (int j = 0; j < 7; j++) { // Inner loop for columns
+		for (int i = 0; i < 7; i++) { 
+			for (int j = 0; j < 7; j++) { 
 				if ((i == 0 ) || (j == 3 ) ||(i == 6)) {
 					System.out.print("*");
 				} else {
