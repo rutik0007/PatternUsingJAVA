092e7be7ecaf58b9437c43cb17546e28f2a59e5b
rutik0007 <rutikbadhe77@gmail.com>
2024-12-18 12:55:32 +0530

    Add files via upload

---
+++ O.java
@@ -0,0 +1,16 @@
+public class O {
+    public static void main(String[] args) {
+        
+        for (int i = 0; i <=7; i++) { 
+            for (int j = 0; j <=7; j++) { 
+                if (j == 0 && i!=0 && i!=7|| j==7 && i!=0 && i!=7 || i==0 && j!=0 && j!=7 || i==7 && j!=0 && j!=7) { 
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+           
+        }
+    }
+}
