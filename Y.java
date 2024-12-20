8233eb459c9df506183b9ac0139d81f98090cfd5
rutik0007 <rutikbadhe77@gmail.com>
2024-12-20 13:37:32 +0530

    Add files via upload

--- Y.java
+++ Y.java
@@ -4,13 +4,13 @@ public class Y {
         for (int i = 0; i <=7; i++) { 
             for (int j = 0; j <=7; j++) { 
                 if ( i==j && j<4 || j==n )  { 
-                    System.out.print("*");
+                    System.out.print("* ");
                 } else {
                     System.out.print(" ");
                 }
             }
             System.out.println();
-           if (i<=7) {
+           if (i<3) {
             n--;
            }
         }
